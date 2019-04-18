2ba4ec3cd1806f3e815a61b7fc13f518b7b8b89f
s1250164 <s1250164@u-aizu.ac.jp>
2019-04-18 13:20:22 +0900

    ex2

--- test.c
+++ test.c
@@ -13,12 +13,10 @@ int main(){
   int **B;  // 道の数を保存
 
   int cross_point = 0; // 交差点の数
-  double **C, *base_C;  // 交差点の座標を保存
-  C = malloc(sizeof(double *));
+  double **C;  // 交差点の座標を保存
 
 
   scanf("%d %d %d %d",&N,&M,&P,&Q);
-
   // 座標の読み込み
   A = malloc(sizeof(int*) * N);
   for(int i=0; i<N; i++){
@@ -41,91 +39,50 @@ int main(){
 
 
   // 交差地点の検出
+  M = 5;
   for(int i=0; i<M-1; i++){
     for(int j=i+1; j<M; j++){
       // printf("i:%d j:%d\n",i,j);
-      double point[2];  // 交差点の座標を一時保存
+      double point[2];
       if(func(A[B[i][0]-1], A[B[i][1]-1], A[B[j][0]-1], A[B[j][1]-1], point)){
         // 交差点の座標を表示
         printf("%.5f %.5f\n",point[0] ,point[1]);
 
-        // // 交差点の座標を追加
-        if(cross_point != 0){ // 配列を拡張
-          double *tmp;
-          tmp = malloc(sizeof(double)*cross_point*2);
-          // puts("malloc tmp");
-          memcpy(tmp, C, sizeof(double)*cross_point*2);
-          // puts("memcpy C->tmp");
-          C = malloc(sizeof(double*) * (cross_point+1)*2);
-          // puts("malloc C");
-          memcpy(C, tmp, sizeof(double)*cross_point*2);
-          // puts("memcpy tmp->C");
-          free(tmp);
-          // puts("free tmp");
-        }
-        C[cross_point] = malloc(sizeof(double) * 2);
-        memcpy(C[cross_point], point, sizeof(point));
 
-        // printf("\nArray C (%d)\n",cross_point);
-        // for(int k=0; k<=cross_point; k++){
-        //   printf("%d: %f %f\n",k,C[k][0], C[k][1]);
-        // }
-        // puts("-----------------------------------");
+        // // 交差点の座標を追加
+        if(cross_point == 0){
+          double **tmp;
+          tmp = malloc(sizeof(double)*cross_point*2);
+          memcpy(tmp, C, sizeof(double)*cross_point*2);
+          C = malloc(sizeof(double*) * (cross_point+1));
+          memcpy(C, tmp, sizeof(double)*cross_point*2);
+          C[cross_point] = malloc(sizeof(double) * 2);
+          memcpy(C[cross_point], point, sizeof(point));
+          free(tmp);
+        }
 
         // // 交差点の数を追加
         cross_point += 1;
       }
-      else{ // 交差点がないとき
-        puts("NA");
-      }
     }
   }
-  // printf("cross_point:%d\n",cross_point);
-  for(int i=0; i<cross_point-2; i++){
-    for(int j=cross_point-1; j>i; j--){
-      // printf("i:%d j:%d   ",i,j);
-      // printf("%f %f\n",C[i][0],C[j][0]);
-      if(C[i][0] > C[j][0]){
-        double *tmp;
-        memcpy(tmp, C[i], sizeof(double)*2);
-        memcpy(C[i], C[j], sizeof(double)*2);
-        memcpy(C[j],tmp, sizeof(double)*2);
-      }
-      else if(C[i][0] == C[j][0] && C[i][1] > C[j][1]){
-        double *tmp;
-        memcpy(tmp, C[i], sizeof(double)*2);
-        memcpy(C[i], C[j], sizeof(double)*2);
-        memcpy(C[j],tmp, sizeof(double)*2);
-      }
 
-    }
-  }
   // for(int i=0; i<cross_point; i++){
   //   printf("%f %f\n",C[i][0], C[i][1]);
   // }
 
-  // メモリ解放
-  for(int i=0; i<N; i++) free(A[i]);
   free(A);
-  // puts("free A");
-  for(int i=0; i<M; i++) free(B[i]);
   free(B);
-  // puts("free B");
-  for(int i=0; i<N; i++) free(C[i]);
-  free(C);
-  // puts("free C");
-
   return 0;
 }
 
 
 int func(int *P1,int *P2, int *Q1, int *Q2, double *point){
-  point[0] = (double)(rand()%20);
-  point[1] = (double)(rand()%20);
+  point[0] = 0.0;
+  point[1] = 2.0;
   // printf("P1:%d %d\n",P1[0],P1[1]);
   // printf("P2:%d %d\n",P2[0],P2[1]);
   // printf("Q1:%d %d\n",Q1[0],Q1[1]);
   // printf("Q2:%d %d\n",Q2[0],Q2[1]);
-  if(point[1] == 12.0) return 0;
   return 1;
 }
