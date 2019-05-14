c8dbcb9b4c9a29667823553a0c36d7b594f2274d
Satomi Nagata <42263270+Satomeme@users.noreply.github.com>
2019-05-14 12:24:02 +0900

    completed shortest_func

--- shortest_func.cpp
+++ shortest_func.cpp
@@ -1,36 +1,54 @@
 #include <stdio.h>
+#include "main.h"
+#include "debug.h"
 
 void shortest(vector<Point>&array,Route P){
-
+    int count =0;
+    
+    if(P->start>array.size()){
+        puts("NA");
+        return;
+    }
+    
+    for(int i=0;i<array.size();i++){
+        array[i].done=false;
+        array[i].cost=-1.0;
+    }
+    array[P->start].cost=0.0;
+    
+    
+    
   while(1){
 
     Point doneNode;
-
+      doneNde.x=-1;
+      
     for(int i=0; array.size()>i;i++){
 
-      if(array[y].done<0 || array[i].cost<0){
-
-	if(doneNode==0 || doneNode.cost<doneNode.cost){
+      if(array[y].done || array[i].cost<0){
+          continue;
+      }
+	if(doneNode.x==-1 || array[i].cost<doneNode.cost){
 	  doneNode=array[i];
 	}
-	  if(doneNode==0) break;
 	}
-      }
-    
+    if(doneNode==-1) break;
+      print_Point(doneNode);
+      
     //確定フラグを立てる
-    doneNode.done=true;
+    array[doneNode.index].done=true;
 
     //接続ノードの情報を更新する
-    for(int j=0;doneNode.next[j].size()-1>j;j++){
-      next[j]=doneNode.next[j];
-      cost=doneNode.next[j]+doneNode.road[j];
+    for(int j=0;doneNode.edges_to.size()>j;j++){
+      int next[j]=doneNode.edges_to[j];
+        double cost=doneNode.edges_to[j]+doneNode.edges_cost[j];
 
-      if(array[next[j]].cost<0 || cost<array[next[j]]).cost){
-      array[next[j].cost=cost;
-	    P.road_data[0].cost=cost;
+      if(array[next[j]].cost<0 || cost<array[next].cost){
+      array[next].cost=cost;
+	    P.route_data[0].cost=cost;
     }
             
     }
-
+  }
   }
             }
